b14473f38bbe12798ca374e05f7d392eea2e68dd
Onur Ersoy <onurersoy10@gmail.com>
2024-05-20 15:40:37 +0300

    cast example added

--- glue-dir/01_notes.py
+++ glue-dir/01_notes.py
@@ -1,3 +1,4 @@
+# FILTERING
 # Filtering doesn't have to be by using '.filter', we can also use '.sql' but we firstly need to convert the ddf to df
 # Converting dynamicframe to dataframe so can use '.createOrReplaceTempView' (We need that to use '.sql') and '.sql'
 AmazonRedshift_node1675772012938.toDF().createOrReplaceTempView("my_vanilla_data_source")
@@ -5,3 +6,6 @@ AmazonRedshift_node1675772012938.toDF().createOrReplaceTempView("my_vanilla_data
 myDataSourceDF = spark.sql("select * from my_vanilla_data_source where cntry_cd = 'TR'")
 # Converting back to dynamicframe
 myDataSourceDynamicFrame = DynamicFrame.fromDF(myDataSourceDF, glueContext, "myDataSourceDynamicFrame")
+
+# CASTING TO DIFFERENT DATA TYPE
+df = df.withColumn("dlvry_last_imported_timestamp", col("dlvry_last_imported_timestamp").cast("string"))
